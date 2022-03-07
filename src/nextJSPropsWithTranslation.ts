111c8655069be74c1a6441bebac684047bf1e57a
the-chat <thechatllc@gmail.com>
2022-03-07 19:37:52 +0500

    simple types

--- src/nextJSPropsWithTranslation.ts
+++ src/nextJSPropsWithTranslation.ts
@@ -4,11 +4,11 @@ import {
   GetStaticProps,
 } from "next"
 import { serverSideTranslations } from "next-i18next/serverSideTranslations"
-import { ArrayElement } from "@the-chat/types"
 import { DEFAULT_LOCALE } from "@the-chat/config"
 
 type A<T, D> = T extends D ? never : T
 
+// TODO?????????????????: locales should not match localesOnEveryPage (ts)
 // todo??????: unique args
 // todo???: delete fn that exists only for types
 // A - Allowed (A<a,b> = if a NOT in b), L - Locale(s), P - Locale(s) on every Page, CL - CurrentLocale(s), GP - GetProps
@@ -17,13 +17,11 @@ const getNJPWT =
   <
     L extends string,
     GP extends GetServerSideProps | GetStaticProps = GetServerSideProps
-  >() =>
-  <Ps extends L[] = []>(localesOnEveryPage: Ps = [] as unknown as Ps) =>
-  <
-    CL extends L = L,
-    CCL extends A<CL, ArrayElement<Ps>> = A<CL, ArrayElement<Ps>>
   >(
-    locales: (CCL extends CL ? CCL[] : never) | never[] = [],
+    localesOnEveryPage: L[] = []
+  ) =>
+  (
+    locales: L[] = [],
     getRestProps: GP = (async () => ({ props: {} })) as unknown as GP
   ): GP =>
     (async (
@@ -50,10 +48,10 @@ export default getNJPWT
 
 // tests
 // type AL = "a" | "b" | "c" | "d" | "e" | "f"
-// const st = getNJPWT<AL, GetStaticProps>()(["a"])
+// const st = getNJPWT<AL, GetStaticProps>(["a"])
 // st(["b"], () => ({ props: {} }))
-// const ssr = getNJPWT<AL, GetServerSideProps>()(["a"])
-// const ssr2 = getNJPWT<AL, GetServerSideProps>()()
+// const ssr = getNJPWT<AL, GetServerSideProps>(["a"])
+// const ssr2 = getNJPWT<AL, GetServerSideProps>()
 // ssr(["b"], async () => ({ props: {} }))
 // ssr2(["b"])
 // ssr2()
